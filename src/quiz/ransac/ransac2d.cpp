2a273a766375633ef4de82fe49f46c21833656a7
yunjin <lineardept@gmail.com>
2020-04-01 20:10:53 +0900

    add ransac3d

--- src/quiz/ransac/ransac2d.cpp
+++ src/quiz/ransac/ransac2d.cpp
@@ -76,6 +76,100 @@ std::unordered_set<int> Ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int ma
 	// If distance is smaller than threshold count it as inlier
 
 	// Return indicies of inliers from fitted line with most inliers
+
+	for(int i;i<maxIteration;i++)
+	{
+		std::unordered_set<int> inliers;
+		while (inlisers.size() < 2)
+			inliers.insert(rand()%(cloud->points.size()));
+
+		float x1, x2, y1, y2;
+
+		auto itr = inliers.begin();
+		x1 = cloud->points[*itr].x;
+		y1 = cloud->points[*itr].y;
+		itr++;
+		x2 = cloud->points[*itr].x;
+		y2 = cloud->points[*itr].y;
+
+		float a = (y2-y1);
+		float b = (x2-x1);
+		float c = (x1*y2 - x2*y1);
+
+		for(int index = 0; index <cloud->points.size(); index++)
+		{
+			if(inliers.count(index)>0)
+				continue;
+
+			pcl::PointXYZ point = cloud->points[index];
+			float x3 = point.x;
+			float y3 = point.y;
+
+			float dist = fabs(a*x3+b*y3+c)/sqrt(a*a + b*b);
+
+			if(dist <= distanceTol)
+				inliers.insert(index);
+		}
+
+		if(inliers.size()>inliersResult.size())
+			inliersResult = inliers;
+	}
+	
+	return inliersResult;
+
+}
+template<typename PointT>
+std::unordered_set<int> Ransac3d(typename pcl::PointCloud<PointT>::Ptr cloud, int maxIterations, float distanceTol)
+{
+	std::unordered_set<int> inliersResult;
+	srand(time(NULL));
+
+	for(int i;i<maxIteration;i++)
+	{
+		std::unordered_set<int> inliers;
+		while (inliers.size() < 3)
+			inliers.insert(rand()%(cloud->points.size()));
+
+		float x1, x2, x3, y1, y2, y3, z1, z2, z3;
+
+		auto itr = inliers.begin();
+		
+		x1 = cloud->points[*itr].x;
+		y1 = cloud->points[*itr].y;
+		z1 = cloud->points[*itr].z;
+		itr++;
+		x2 = cloud->points[*itr].x;
+		y2 = cloud->points[*itr].y;
+		z2 = cloud->points[*itr].z;
+		itr++;
+		x3 = cloud->points[*itr].x;
+		y3 = cloud->points[*itr].y;
+		z3 = cloud->points[*itr].z;
+
+		float a = (y2-y1)*(z3-z1)-(y3-y1)*(z2-z1);
+		float b = (z2-z1)*(x3-x1)-(z3-z1)*(x2-x1);
+		float c = (x2-x1)*(y3-y1)-(x3-x1)*(y2-y1);
+		float d = -(a*x1+b*y1+c*z1);
+
+		for(int index = 0; index <cloud->points.size(); index++)
+		{
+			if(inliers.count(index)>0)
+				continue;
+
+			pcl::PointXYZ point = cloud->points[index];
+			float x = point.x;
+			float y = point.y;
+			float z = point.z;
+
+			float dist = fabs(a*x+b*y+c*z+d)/sqrt(a*a + b*b + c*c);
+
+			if(dist <= distanceTol)
+				inliers.insert(index);
+		}
+
+		if(inliers.size()>inliersResult.size())
+			inliersResult = inliers;
+	}
 	
 	return inliersResult;
 
