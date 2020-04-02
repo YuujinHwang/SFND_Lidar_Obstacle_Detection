a662d594cf65cb3206c9eb0eee6b02da192bb480
yunjin <lineardept@gmail.com>
2020-04-02 16:04:39 +0900

    test

--- src/environment.cpp
+++ src/environment.cpp
@@ -87,7 +87,7 @@ void cityBlock(pcl::visualization::PCLVisualizer::Ptr& viewer, ProcessPointCloud
     // std::pair<pcl::PointCloud<pcl::PointXYZI>::Ptr, pcl::PointCloud<pcl::PointXYZI>::Ptr> segmentCloud = pointProcessorI->SegmentPlane(filterCloud,100,0.5);
     // std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> cloudClusters = pointProcessorI->Clustering(segmentCloud.first, 0.5, 10, 10000);
     
-    std::unordered_set<int> inliers = Ransac3d(filterCloud, 100, 0.2);
+    std::unordered_set<int> inliers = Ransac3d(filterCloud, 50, 0.3);
     pcl::PointCloud<pcl::PointXYZI>::Ptr cloudPlane(new pcl::PointCloud<pcl::PointXYZI>());
     pcl::PointCloud<pcl::PointXYZI>::Ptr cloudObstacle(new pcl::PointCloud<pcl::PointXYZI>());
 
@@ -121,7 +121,7 @@ void cityBlock(pcl::visualization::PCLVisualizer::Ptr& viewer, ProcessPointCloud
         ++clusterId;
     }
     
-    
+    renderPointCloud(viewer, cloudObstacle,"obstCloud", Color(0,0,1));
     renderPointCloud(viewer, cloudPlane,"planeCloud",Color(0,1,0));
 
     // int clusterId = 0;
